49d2e042255d96514e017b73fc7417a6b7aff8d1
Dhanjay sah <dhanjaysah838@gmail.com>
2025-01-27 10:07:14 +0530

    Add files via upload

---
+++ python/sah,.py
@@ -0,0 +1,87 @@
+######COMMENTS 
+# print("hi")
+# #escape
+# print("i am a good boy \n and viwer is also good boy")
+# #print
+# # print("hi",6,7,sep="~",end="000\n")
+# # # print("sah")
+
+
+
+#####VARIBLES AND DATA TYPES
+# a = 1
+# b = True
+# c = "sah"
+# d = None
+# print(c)
+# # #data types int,float,complex,boolean,string
+# a = 1
+# b = 1.2
+# c = complex(2, 7)
+# d = False
+# e = "sah"
+# print(c)
+# # #sequenced data:list,tuple
+# list = [8, 2.5, ["apple"]]
+# print(list)
+# tuple = ("parrot", "sah"), ("bihar", "tiger")
+# print(tuple)
+
+#mapped datatype:dict
+# dict = {"name": "Dhanjay", "age": 20}
+# print(dict)
+
+###TYPE CASTING(conversion of data type)
+# a="4"
+# b="5"
+# print=(a+b)          #here the o/p is 45 beacuse 4&5 are string so if you want to add them then you have to convert them into integer
+# print(int(a)+int(b))
+
+
+###HOW TO TAKE INPUT FROM USER
+# a=input("Enter your name:")
+# b=input("enter the first number:")
+# c=input("enter the second number:")
+# print(b+c)
+# print(float(b)+float(c))
+# print(complex(b)+complex(c))
+
+
+###STRINGS
+# user=('my name is sambha')
+# user1=("i want to sea arkestra")
+# sah=("""i am good boy
+# and i am studying in b.tech
+#   in the stream of computer scienece""")
+# print(user)
+# print(user1)
+# print(sah)
+# ##INDEXING
+# print(user[0])
+# for character in sah:## for loop used for print all the character of the string
+#   print(character)
+
+
+###STRING SLICING
+# sah="Dhanjay sah"
+# a=len(sah)
+# print(a)
+# print(sah[0:5])
+# print(sah[8])
+# print(sah[:-2])
+# b=("apple is good for health")
+# print(b[-1:4])
+# for character in b:
+#    print(character)
+
+
+####STRINGS METHODS ##TASK:1
+A="this is python class day 3"
+# A=(A.capitalize())
+# print(A.split())
+# A=A.replace("y","Y",1)
+# print(A.replace(" ","-"))
+
+
+print(A[:-2])
+
